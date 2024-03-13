501a99bb1ba600f391b92863550cd1665d8b0e4a
Carmen <my-9@hotmail.it>
2024-03-13 16:50:43 +0100

    first commit

---
+++ toDoList.js
@@ -0,0 +1,43 @@
+let form = document.getElementById("formToDo");
+let inputTasks = document.getElementById("inputTasks");
+let btnTasks = document.getElementById("btnTasks");
+let containerTask = document.getElementById("containerTask");
+
+const task = (e) => {
+  inputTasks.addEventListener("change", (event) => {
+    let text = event.target.value;
+  });
+};
+
+btnTasks.onclick = (e) => {
+  e.preventDefault();
+  let divTask = document.createElement("div");
+  divTask.className = "divTask";
+  let newTask = document.createElement("h3");
+  let text = inputTasks.value;
+  if (text.length > 0) {
+    newTask.innerText = text;
+
+    newTask.addEventListener("click", (e) => {
+      newTask.style.textDecoration = " line-through ";
+      newTask.style.backgroundColor = " green ";
+    });
+
+    let deleteTask = document.createElement("button");
+    deleteTask.type = "button";
+    deleteTask.style.backgroundColor = "#d20000";
+    deleteTask.style.color = "white";
+    deleteTask.innerText = "Delete task";
+    deleteTask.addEventListener("click", (e) => {
+      divTask.remove();
+    });
+
+    divTask.appendChild(newTask);
+    divTask.appendChild(deleteTask);
+    containerTask.appendChild(divTask);
+
+    inputTasks.value = "";
+  }
+};
+
+form.addEventListener("submit", task);
