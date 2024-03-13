d043f1442025aa4a8f3d24e7c18df6cd79a317d5
Carmen <my-9@hotmail.it>
2024-03-13 18:10:31 +0100

    formOnSubmit

--- toDoList.js
+++ toDoList.js
@@ -3,35 +3,30 @@ let inputTasks = document.getElementById("inputTasks");
 let btnTasks = document.getElementById("btnTasks");
 let containerTask = document.getElementById("containerTask");
 
-btnTasks.onclick = (e) => {
+form.onsubmit = (e) => {
   e.preventDefault();
   let divTask = document.createElement("div");
   divTask.className = "divTask";
   let newTask = document.createElement("h3");
-  let text = inputTasks.value;
-  if (text.length > 0) {
-    newTask.innerText = text;
+  newTask.innerText = inputTasks.value;
 
-    newTask.addEventListener("click", (e) => {
-      newTask.style.textDecoration = " line-through ";
-      newTask.style.backgroundColor = " green ";
-    });
+  newTask.addEventListener("click", (e) => {
+    newTask.style.textDecoration = " line-through ";
+    newTask.style.backgroundColor = " green ";
+  });
 
-    let deleteTask = document.createElement("button");
-    deleteTask.type = "button";
-    deleteTask.style.backgroundColor = "#d20000";
-    deleteTask.style.color = "white";
-    deleteTask.innerText = "Delete task";
-    deleteTask.addEventListener("click", (e) => {
-      divTask.remove();
-    });
+  let deleteTask = document.createElement("button");
+  deleteTask.type = "button";
+  deleteTask.style.backgroundColor = "#d20000";
+  deleteTask.style.color = "white";
+  deleteTask.innerText = "Delete task";
+  deleteTask.addEventListener("click", (e) => {
+    divTask.remove();
+  });
 
-    divTask.appendChild(newTask);
-    divTask.appendChild(deleteTask);
-    containerTask.appendChild(divTask);
+  divTask.appendChild(newTask);
+  divTask.appendChild(deleteTask);
+  containerTask.appendChild(divTask);
 
-    inputTasks.value = "";
-  }
+  inputTasks.value = "";
 };
-
-form.addEventListener("submit", task);
